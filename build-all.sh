3972485f59eb8a72bffc86503f3faca51b28f696
decaby7e <27081624+decaby7e@users.noreply.github.com>
2020-07-22 22:50:01 -0400

    Build script updates

--- build-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#docker build --build-arg VERSION=1.14.4 -t ranvier/spigot-mc:1.14.4 .
-docker build --build-arg VERSION=1.15.2 -t ranvier/spigot-mc:1.15.2 .
-docker build --build-arg VERSION=1.16.1 -t ranvier/spigot-mc:1.16.1 .
-docker build --build-arg VERSION=latest -t ranvier/spigot-mc:latest .
-
-exit 0
---
+++ wizard.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ACTION=$1
+SAVE_DIR='./build/'
+
+build(){
+  docker build --build-arg VERSION=1.16.1 -t ranvier/spigot-mc:1.16.1 .
+  docker build --build-arg VERSION=latest -t ranvier/spigot-mc:latest .
+}
+
+save(){
+  mkdir $SAVE_DIR
+  docker image save ranvier/spigot-mc:1.16.1 > $SAVE_DIR/spigot-mc-1.16.1.tar
+  docker image save ranvier/spigot-mc:latest > $SAVE_DIR/spigot-mc-latest.tar
+}
+
+if [[ "$ACTION" == 'build' ]]; then
+  build
+elif [[ "$ACTION" == 'save' ]]; then
+  save
+else
+  build && save
+fi
+
+exit 0
