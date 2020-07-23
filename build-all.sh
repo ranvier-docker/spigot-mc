579a733e323c93fd40861dbe637215a7a49acff5
decaby7e <27081624+decaby7e@users.noreply.github.com>
2020-07-22 22:42:51 -0400

    Build script

--- build-all.sh
+++ build-all.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-docker build --build-arg VERSION=1.12.2 -t ranvier/spigot-mc:1.12.2 .
-docker build --build-arg VERSION=1.13.2 -t ranvier/spigot-mc:1.13.2 .
-docker build --build-arg VERSION=1.14.4 -t ranvier/spigot-mc:1.14.4 .
+#docker build --build-arg VERSION=1.14.4 -t ranvier/spigot-mc:1.14.4 .
 docker build --build-arg VERSION=1.15.2 -t ranvier/spigot-mc:1.15.2 .
+docker build --build-arg VERSION=1.16.1 -t ranvier/spigot-mc:1.16.1 .
 docker build --build-arg VERSION=latest -t ranvier/spigot-mc:latest .
 
 exit 0
